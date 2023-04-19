f70813492ff849c8c256cdd8e1c5b9adee289d26
Roman Marfutin <r.marfutn@sh.dunice.net>
2023-04-19 16:30:49 +0300

    added attempt to make several threads to file searching

--- Program.cs
+++ Program.cs
@@ -2,19 +2,41 @@
 using System.IO;
 using System.Threading;
 using System.Collections;
+using static System.Environment;
+
 
 string sourceDirectory = @"/home/dunice/tfolder/tfold2";
 string requiredFile = "file5.txt";
-Searcher.StepByFolders(sourceDirectory, requiredFile);
+int processorCount = Environment.ProcessorCount;
 
-// Thread t1 = Thread.CurrentThread;
+for (int i = 1; i <= processorCount; i++)
+{
+    Searcher searcher = new Searcher(i, sourceDirectory, requiredFile, processorCount);
+}
 
 class Searcher
 {
-    static public void StepByFolders(string path, string filname)
+    static Semaphore sem = new Semaphore(1, ProcessorCount);
+    string RequiredFile {set;get;}
+    string Path {set;get;}
+    static int ProcessorCount;
+
+    Thread myThread;
+    public Searcher (int i, string path, string requiredFile, int processorCount)
     {
-        string[] hereFiles = Directory.GetFiles(path);
-        string[] hereFolders = Directory.GetDirectories(path);
+        RequiredFile = requiredFile;
+        Path = path;
+        ProcessorCount = processorCount;
+
+        myThread = new Thread(StepByFolders);
+        myThread.Name = $"Searcher №{i}";
+        myThread.Start();
+    } 
+
+    public void StepByFolders()
+    {
+        string[] hereFiles = Directory.GetFiles(Path);
+        string[] hereFolders = Directory.GetDirectories(Path);
 
         List<FileInfo> currentFiles = new List<FileInfo>();
         List<string> fileNames = new List<string>();
@@ -25,24 +47,64 @@ class Searcher
             fileNames.Add(currentFiles[s].Name);
         }
 
-        if (fileNames.Contains(filname))
+        if (fileNames.Contains(RequiredFile))
         {
-            int foundedIndex = fileNames.IndexOf(filname);
+            int foundedIndex = fileNames.IndexOf(RequiredFile);
             Console.WriteLine($"We found it, nice! Your file is here: '{hereFiles[foundedIndex]}'");
             Environment.Exit(0);
         }
         else
         {
+            
             for (int s = 0; s < hereFolders.Length; s++)
             {
-                StepByFolders(hereFolders[s], filname);
-
+                sem.WaitOne();
+                StepByFolders();
+                Thread.Sleep(1000);
+                sem.Release();
+                Thread.Sleep(1000);
+                
                 if (s == hereFolders.Length - 1)
                 {
                     Console.WriteLine($"Sorry, I can't find your file here");
                     Environment.Exit(0);
                 }
             }
+
         }
+    }
+}
+
+
+class Reader
+{
+    static Semaphore sem = new Semaphore(3, 3);
+    Thread myThread;
+    int count = 6;
+
+    public Reader(int i)
+    {
+        myThread = new Thread(Read);
+        myThread.Name = $"Читатель {i}";
+        myThread.Start();
+    }
+
+    public void Read()
+    {
+        while (count > 0)
+        {
+            sem.WaitOne();
+
+            Console.WriteLine($"{Thread.CurrentThread.Name} входит в библиотеку");
+            Console.WriteLine($"{Thread.CurrentThread.Name} читает");
+            Thread.Sleep(1000);
+
+            Console.WriteLine($"{Thread.CurrentThread.Name} покидает библиотеку");
+            sem.Release();
+
+            count--;
+            Thread.Sleep(1000);
+        }
+
     }
 }
